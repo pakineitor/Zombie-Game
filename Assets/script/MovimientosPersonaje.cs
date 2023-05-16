eb5b4923e4249a4de91d0cfe8d52d3185e36c54e
franciscorey-alumn <franciscojavierrey.alumn@cescristorey.com>
2023-05-16 23:44:45 +0200

    Versión 0.8 16.5.23
    Se han eliminado y renovado las animaciones.

--- Assets/script/MovimientosPersonaje.cs
+++ Assets/script/MovimientosPersonaje.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class MovimientosPersonaje : MonoBehaviour
 {
+
+    Animator animacion;                    
+    
+    public void Saltar()
+    {
+        if (Input.GetButtonDown("Jump")){
+            animacion.SetTrigger("saltar");
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animacion = GetComponent<Animator>();                                   //--------Agregamos una referencia al animator
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Saltar();
     }
 }
