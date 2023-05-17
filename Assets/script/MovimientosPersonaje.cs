ecc9e26e274ac10f610c51f5ea97d34fd65196ce
franciscorey-alumn <franciscojavierrey.alumn@cescristorey.com>
2023-05-17 20:43:05 +0200

    Versión 0.8.1
    En esta versión se han completado todas las animaciones posibles básicas del personaje.
    Se ha factorizado el código.
    En la próxima versión, se empezará a desarrollar el primer nivel.

--- Assets/script/MovimientosPersonaje.cs
+++ Assets/script/MovimientosPersonaje.cs
@@ -6,19 +6,19 @@ public class MovimientosPersonaje : MonoBehaviour
 {
 
     Animator animacion;
-    Rigidbody2D rigidbody2;                                                                 //--------Vamos a referenciarle el Rigibody para que apunte al aplicado al personaje desde el editor. De esta manera podremos aplicarle las fuerzas necesarias para hacer real un salto.
+    Rigidbody2D rigidbody2;                                                                                             //--------Vamos a referenciarle el Rigibody para que apunte al aplicado al personaje desde el editor. De esta manera podremos aplicarle las fuerzas necesarias para hacer real un salto.
     public float fuerzaSalto;
     public bool isSuelo;
     public Transform refPie;
     float movX;
     public float velx;
-    /// <summary>
-    /// Función que va a ejecutar el salto.
-    /// </summary>
+                                                                                                                        /// <summary>
+                                                                                                                        /// Función que va a ejecutar el salto.
+                                                                                                                        /// </summary>
     public void Saltar()
     { 
-            rigidbody2.AddForce(new Vector2(0, fuerzaSalto), ForceMode2D.Impulse);           //--------Si pulsamos saltar le añadimos una fuerza con un vector2 y así poder pasarle la fuerza en ambos ejes, y le pasamos el tipo de fuerza que en este caso es impulso.
-            animacion.SetTrigger("saltar");                                                  //--------Establecemos la acción de saltar si presionamos la tecla Jump (barra espaciadora).
+            rigidbody2.AddForce(new Vector2(0, fuerzaSalto), ForceMode2D.Impulse);                                      //--------Si pulsamos saltar le añadimos una fuerza con un vector2 y así poder pasarle la fuerza en ambos ejes, y le pasamos el tipo de fuerza que en este caso es impulso.
+            animacion.SetTrigger("saltar");                                                                             //--------Establecemos la acción de saltar si presionamos la tecla Jump (barra espaciadora).
         
     }
 
@@ -27,23 +27,23 @@ public class MovimientosPersonaje : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        animacion   = GetComponent<Animator>();                                             //--------Agregamos una referencia al animator
-        rigidbody2  = GetComponent<Rigidbody2D>();                                          //--------Aquí referenciamos el componente externo con la variable de tipo RigiBody2d.
+        animacion               = GetComponent<Animator>();                                                             //--------Agregamos una referencia al animator
+        rigidbody2              = GetComponent<Rigidbody2D>();                                                          //--------Aquí referenciamos el componente externo con la variable de tipo RigiBody2d.
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        rigidbody2.velocity = new Vector2(velx * movX, rigidbody2.velocity.y);
-        movX = Input.GetAxis("Horizontal"); 
-        animacion.SetFloat("MoveX", Mathf.Abs(movX));
-        isSuelo = Physics2D.OverlapCircle(refPie.position, 1f, 1 << 8);
-        animacion.SetBool("isPiso", isSuelo);
+        rigidbody2.velocity      = new Vector2(velx * movX, rigidbody2.velocity.y);                                     //--------Referenciamos el componente Rigidbody2d con un vector que contiene una velocidad por un movimiento en el eje x y una coordenadas en el eje y.
+        movX                     = Input.GetAxis("Horizontal");                                                         //--------Extraemos el axis horizontal para el movimiento en el eje x con -1 o 1. 
+        animacion.SetFloat("MoveX", Mathf.Abs(movX));                                                                   //--------Establecemos un float en valor absoluto para que cuando e mueva en sentido negativo, no haya errores pero Unity sepa disntinguirlos.
+        isSuelo                  = Physics2D.OverlapCircle(refPie.position, 1f, 1 << 8);                                //--------En esta sentencia de código, guardamos un bool comparando que si hay algo en el área formada por el radio de refPie, que sea true o false.
+        animacion.SetBool("isPiso", isSuelo);                                                                           //--------Referenciamos parámetro del animator llamado isPisom con el valor booleano guardado en la variable isSuelo.
 
-        if (Input.GetButtonDown("Jump") && isSuelo)                  //--------Si está en el sueloy pulso saltar:
+        if (Input.GetButtonDown("Jump") && isSuelo)                                                                     //--------Si está en el sueloy pulso saltar:
         {
-            Saltar();                                                //--------Establecemos la acción de saltar si presionamos la tecla Jump (barra espaciadora).
+            Saltar();                                                                                                   //--------Establecemos la acción de saltar si presionamos la tecla Jump (barra espaciadora).
         }
 
     }
