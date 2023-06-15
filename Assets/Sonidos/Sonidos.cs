f9af63fc13a6ba27e752348a45f2ffb30edc72b3
franciscorey-alumn <franciscojavierrey.alumn@cescristorey.com>
2023-06-15 23:46:44 +0200

    Versión 0.18 del 15.6.23
    He añadido assets de audio para empezar a trabajar con ellos.

---
+++ Assets/Sonidos/Sonidos.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sonidos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource audioSource;
+}
--- Assets/script/Screen/ScreenPause.cs
+++ Assets/script/Screen/ScreenPause.cs
@@ -9,7 +9,8 @@ public class ScreenPause : MonoBehaviour
     public GameObject PausaMenu;
     public GameObject PantallaControles;
     public GameObject Pakineitor;
-
+    public GameObject bt_mutear;
+    public GameObject bt_desMutear;
     public void Pausar()
     {
        
@@ -46,5 +47,19 @@ public class ScreenPause : MonoBehaviour
     public void VolverMenuPausa()
     {
         PantallaControles.SetActive(false);
+    }
+
+    public void MutearSonido()
+    {
+        //Desactivas el sonido del nivel.
+        bt_mutear.SetActive(false);
+        bt_desMutear.SetActive(true);
+    }
+
+    public void DesmutearSonido()
+    {
+        //Activar sonido
+        bt_mutear.SetActive(true);
+        bt_desMutear.SetActive(false);
     }
 }
